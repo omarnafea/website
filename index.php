7482cfd052b9577b725d11b9ab7985776ff02b1e
omarnafea <48605765+omarnafea@users.noreply.github.com>
2019-10-26 23:26:32 +0300

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,42 @@
 </head>
 <body>
 
-	<?php include'include/template/navbar.php'; ?>
+	<!-- start Nav-bar-->
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+   <div class="container">
+    <div class="text_left">
+      <h4> OMAR NAFE'</h4>
+    </div>
+    
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main_nav" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="main_nav">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active ">
+          <a class="nav-link " href="#">HOME </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="#about">ABOUT<span class="sr-only">(current)</span></a>
+        </li>
+         <li class="nav-item">
+          <a class="nav-link" href="#services">SERVICES</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#contact">CONTACT</a>
+        </li>
+        
+        
+        
+      </ul>
+      
+    </div>
+  </div>
+</nav>
+
+
+
+<!-- end Nav-bar-->
 
 
 
