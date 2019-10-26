a65aaf4e4de835f7f9ce3246ef627722fde39e20
omarnafea <onafea08@gmail.com>
2019-10-26 22:59:09 +0300

    all

---
+++ index.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Omar Nafea</title>
+	<script src="layout/js/jquery-3.4.1.min.js"></script>
+    <link rel="stylesheet" href="layout/css/bootstrap.min.css" />
+    <script src="https://kit.fontawesome.com/9bb4e0493f.js" crossorigin="anonymous"></script>
+ 
+  <script src="layout/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="layout/css/main.css">
+  <link rel="stylesheet" type="text/css" href="layout/css/font-awesome.min.css">
+</head>
+<body>
+
+	<?php include'include/template/navbar.php'; ?>
+
+
+
+	<div class="container">
+
+		<div class="text-center me">
+			<h1> HI! I'M OMAR NAFE'</h1>
+			<h6> I AM A WEB DEVELOPER !</h6>
+		</div>
+
+		<div>
+    
+</div>
+
+
+
+
+		<div class="about" id="about">
+			<h2 class="text-center">About Me</h2>
+
+			<div class="row">
+				<div class="col-md-6 col-sm-12">
+				    <img src="layout/img/omar.jpg" class="img-fluid rounded-circle">
+			    </div>
+				<div class="col-md-6 col-sm-12">
+					<p>My name is <span>OMAR NAFE'</span>  and welcome to my website!</p>
+
+					<p> I am a <span>web developer</span>  and my main job is to help people's businesses reach world wide by developing a high end creative websites designs.</p>
+
+					<p>In this webpage you'll find my skills and qualifications.
+					As well as, some of people's recommendations about my work.</p>
+
+					<p>Enjoy!</p>
+
+					<p><i class="fas fa-phone-alt"></i>CALL: 0795496663</p>
+					<p><i class="fas fa-envelope-square"></i>EMAIL: Onafea08@gmail.com</p>
+					<ul class="list-unstyled">
+					<li><a href="https://www.facebook.com/omar.nafea.97"><i class="fab fa-facebook-f"></i></i></a></li>
+					<li><a href="https://www.linkedin.com/in/omar-nafea-a78187147/"><i class="fab fa-linkedin"></i></a></li>
+					</ul>
+					<object data="omar_nafea_cv.pdf" type="application/pdf" width="300" height="200">
+                     <a class="btn btn-primary" href="omar_nafea_cv.pdf">View Resume</a>
+                    </object>
+					
+
+				</div>
+			</div>
+		</div>
+
+</div>
+
+<div class="container"> 
+		<div class="services" id="services">
+			<h2 class="text-center">My Services</h2>
+
+			<div class="row" >
+				<div class="col-md-4 col-sm-12 text-center">
+					<div class="text-center"> <i class="fas fa-code"></i></div>
+					<h3 class="text-center">Web Development</h3>
+					<p  class="text-center">Ease of Use and Reliable Systems</p>
+				    
+			    </div>
+			    <div class="col-md-4 col-sm-12 text-center">
+			    	<div class="text-center"> <i class="fas fa-laptop"></i></div>
+					<h3>UI/UX Design</h3>
+					<p>Smooth and Catchy Designs</p>
+				    
+			    </div>
+			    <div class="col-md-4 col-sm-12 text-center">
+			    <div class="text-center"><i class="far fa-clock"></i></div>	
+					<h3>On Time Delivery</h3>
+					<p>Extra features for each day of delay.</p>
+				    
+			    </div>
+			    <div class="col-md-4 col-sm-12 text-center">
+			    	<div class="text-center"><i class="fas fa-search"></i></div>
+					<h3>Search Engine Optimization</h3>
+					<p>ncrease your website rank and engagement by getting the latest SEO updates and tools.</p>
+				    
+			    </div>
+			    <div class="col-md-4 col-sm-12 text-center">
+			    	<div class="text-center"><i class="fas fa-mobile-alt"></i></div>
+					<h3>Responsive Design</h3>
+					<p>One design supported by all platforms.</p>
+				    
+			    </div>
+			    <div class="col-md-4 col-sm-12 text-center">
+			    	<div class="text-center"><i class="fas fa-headset"></i></div>
+					<h3>Support</h3>
+					<p>I offer technical support, backup and modifications.</p>
+				    
+			    </div>
+				
+			</div>
+		</div>
+
+
+          <div class="skills">
+			<h2 class="text-center">Technology Skills</h2>
+
+			<div class="row">
+				<div class="col-md-4 col-sm-12">
+					<p>HTML5, CSS, SCSS, WORDPRESS</p>
+					<div class="progress">
+						
+					       <div class="progress-bar" role="progressbar" style="width: 90%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+					      90%
+					     </div>
+					</div>
+				    
+			    </div>
+			    <div class="col-md-4 col-sm-12">
+			    	<p>SEO</p>
+					<div class="progress">
+					       <div class="progress-bar" role="progressbar" style="width: 90%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+					      70%
+					     </div>
+					</div>
+				    
+			    </div>
+			    <div class="col-md-4 col-sm-12">
+			    	<p>BOOTSTRAP, JQUERY, AJAX</p>
+					<div class="progress">
+					       <div class="progress-bar" role="progressbar" style="width: 90%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+					      95%
+					     </div>
+					</div>
+				    
+			    </div>
+			    <div class="col-md-4 col-sm-12">
+			    	<p>PHP,MYSQL</p>
+					<div class="progress">
+					       <div class="progress-bar" role="progressbar" style="width: 90%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+					      95%
+					     </div>
+					</div>
+				    
+			    </div>
+			    <div class="col-md-4 col-sm-12">
+			    	<p>TEAM MANAGEMENT</p>
+					<div class="progress">
+					       <div class="progress-bar" role="progressbar" style="width: 90%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+					      90%
+					     </div>
+					</div>
+			    </div>
+			    <div class="col-md-4 col-sm-12">
+			    	<p>UI/UX</p>
+					<div class="progress">
+					       <div class="progress-bar" role="progressbar" style="width: 90%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+					      80%
+					     </div>
+					</div>
+				    
+			    </div>
+				
+			</div>
+		</div>
+
+		<div class="statitics text-center" id="contact">
+			<div class="row">
+			    <div class="col-md-4 col-sm-12">
+			    	<div class="text-center"> <i class="fas fa-laptop"></i></div>
+					<h3>PROJECTS DONE</h3>
+					<p class="number">6</p>
+				    
+			    </div>
+
+			    <div class="col-md-4 col-sm-12">
+					<p>E_commerce System </p>
+					<p>HR System</p>
+					<p>Smart Pharmacy</p>
+					<p>Travel AND Food Blog</p>
+					<p>Competition System</p>
+					<p>Library System</p>
+				    
+			    </div>
+			    <div class="col-md-4 col-sm-12">
+			    	<div class="text-center"> <i class="fas fa-smile-beam"></i></div>
+					<h3>HAPPY CLIENTS</h3>
+					<p class="number">6</p>
+				    
+			    </div>
+			</div>
+			    
+				
+			
+		</div>
+
+
+		<div class="contact text-center">
+			<h2>Contact Me</h2>
+			<div class="row">
+			    <div class="col-md-4 col-sm-12">
+			    	<i class="fas fa-phone-alt"></i>
+			    	<p> 0795496663</p>
+					
+				    
+			    </div>
+
+			    <div class="col-md-4 col-sm-12">
+					<i class="fas fa-envelope-square"></i>
+					<p>Onafea08@gmail.com</p>
+				    
+			    </div>
+			    <div class="col-md-4 col-sm-12">
+			    	<i class="fas fa-map-marker-alt"></i>
+					<p>Amman/tabarbour</p>
+				    
+			    </div>
+			</div>
+			    
+				
+			
+		</div>
+
+		<div class="copy-right">
+	<div class="container">
+		<div class="row ">
+			<div class="col-sm-6  text-center text-sm-left text-uppercase">
+			  Copyright 2019 Omar Nafe' &copy; all rghts reserved</div>
+			<div class="col-sm-6 text-center text-sm-right">
+			  <ul class="list-unstyled">
+			    <li><a href="https://www.facebook.com/omar.nafea.97"><i class="fab fa-facebook-f"></i></i></a></li>
+				<li><a href="https://www.linkedin.com/in/omar-nafea-a78187147/"><i class="fab fa-linkedin"></i></a></li>
+			  </ul>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+
+
+	</div>
+
+</body>
+</html>
